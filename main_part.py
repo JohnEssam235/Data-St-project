d0cc6a555a70a1c6f755fec4466d9313e42bd27c
JohnEssam235 <56760033+JohnEssam235@users.noreply.github.com>
2020-05-28 01:17:09 +0200

    Update main_part.py

--- main_part.py
+++ main_part.py
@@ -1,21 +1,47 @@
-root = build_tree(df)
-dev = pd.read_csv('sample_dev.csv')
-dev = dev.drop(columns=['reviews.text'])
+# Reading train and building the decision tree
+train_df = pd.read_csv('sample_train.csv')
+train_df = train_df.drop(columns=['reviews.text'])
+print('Building Tree...')
+root = build_tree(train_df)
+print('Tree was built successfully')
+print()
 
+# Evaluating the accuracy by traversing the tree using the train sample
+print('Processing Train...')
 count = 0
 output = []
-for index, row in dev.iterrows():
-	if root.traverse(row) == 1:
-		output.append('Positive')
-
-	else:
-		output.append('Negative')
-
-	if dev.iloc[index, -1] == output[-1]:
+for i, row in train_df.iterrows():
+	output.append(root.traverse(row))
+	if train_df.iloc[i, -1] == output[-1]:
 		count = count + 1
+print('Accuracy = {:0.2f}%' .format((count / len(train_df.index) * 100)))
+print('Train processed successfully')
+print()
 
-print('Accuracy = {:0.2f}%' .format((count / len(dev.index) * 100)))
-
+# Predicting the ratings of the test sample
+test_df = pd.read_csv('sample_test.csv')
+print('Processing Test...')
+count = 0
+output = []
+for i, row in test_df.iterrows():
+	output.append(root.traverse(row))
 label = {'rating': output}
 output_df = pd.DataFrame(label)
-output_df.to_csv('result.csv')
+output_df.to_csv('result.csv', index=False)
+print('Predictions saved to (result.csv)')
+
+# Predicting the result of a user entered case
+while True:
+	print('Try new case? (Y/N)')
+	choice = input()
+	if choice == 'N' or choice == 'n':
+		break
+	else:
+		print('Enter new case:')
+		case = {}
+		for i in range(0, 25):
+			col_name = train_df.columns[i]
+			case[col_name] = int(input())
+		case = pd.Series(case)
+		print('Prediction: ', root.traverse(case))
+		print()
