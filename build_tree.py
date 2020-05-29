51a3ea0597f756eef2dc2f82fa1d5fe7099ccc64
Khalidm98 <62033170+Khalidm98@users.noreply.github.com>
2020-05-29 10:11:04 +0200

    Update and rename build_tree_func.py to build_tree.py

--- build_tree.py
+++ build_tree.py
@@ -3,10 +3,10 @@ def build_tree(data):
 	if data.empty:
 		return root
 
-	index, decision = calc_gain(data)
+	index, decision = get_decision(data)
 	root.column = index
 	root.decision = decision
-	root.childs = np.empty((1, 0), Node)
+	root.children = np.empty((1, 0), Node)
 
 	# pure decision
 	if decision != -1:
@@ -14,13 +14,13 @@ def build_tree(data):
 		if decision < 2:
 			data0 = data.loc[data[data.columns[index]] == 0]		# get all rows where word == 0
 			data0 = data0.drop(columns=[data.columns[index]])
-			root.childs = np.append(root.childs, np.array([build_tree(data0)]))
+			root.children = np.append(root.children, np.array([build_tree(data0)]))
 
 		# if decision is pure when word = 0
 		elif decision < 4:
 			data1 = data.loc[data[data.columns[index]] == 1]		# get all rows where word == 1
 			data1 = data1.drop(columns=[data.columns[index]])
-			root.childs = np.append(root.childs, np.array([build_tree(data1)]))
+			root.children = np.append(root.children, np.array([build_tree(data1)]))
 
 		# if decision is pure at both 1 and 0
 		elif decision < 8:
@@ -29,11 +29,10 @@ def build_tree(data):
 	else:
 		data0 = data.loc[data[data.columns[index]] == 0]			# get all rows where word == 0
 		data0 = data0.drop(columns=[data.columns[index]])
-		root.childs = np.append(root.childs, np.array([build_tree(data0)]))
+		root.children = np.append(root.children, np.array([build_tree(data0)]))
 
 		data1 = data.loc[data[data.columns[index]] == 1]			# get all rows where word == 1
 		data1 = data1.drop(columns=[data.columns[index]])
-		root.childs = np.append(root.childs, np.array([build_tree(data1)]))
+		root.children = np.append(root.children, np.array([build_tree(data1)]))
 
 	return root
-	
